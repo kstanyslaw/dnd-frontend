dedefe6e21a62a13eda3602381caefa7c9c08832
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2020-01-26 16:36:17 +0700

    Add Dnd component and routing to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DndComponent } from './dnd/dnd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DndComponent
   ],
   imports: [
     BrowserModule,
