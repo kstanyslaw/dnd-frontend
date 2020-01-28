d9efc2271a9d17fd5714a983617df0084cf8572c
Stan <k.stanyslaw@gmail.com>
2020-01-28 13:08:52 +0700

    Can set multiple files and get its preview. Add-to-form don't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dnd-frontend';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  imagePreviews: string[];
+
+  onSave() {
+    console.log({
+      files: this.form.value.files
+    });
+    this.form.reset();
+  }
+
+  onImagePicked(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    this.form.patchValue({files: Array.from(files)});
+    this.form.get('files').updateValueAndValidity();
+
+    //Preview
+    this.imagePreviews = [];
+    Array.from(files).forEach(element => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreviews.push(String(reader.result));
+      };
+      reader.readAsDataURL(element);
+    });
+    console.log(Array.from(files));
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      files: new FormArray([new FormControl(null, {validators: [Validators.required]})])
+    });
+  }
 }
